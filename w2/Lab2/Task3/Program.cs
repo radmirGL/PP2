26dd8b77be751d85c0bc5030d92c0e9183a62a80
radmirGL <DzSee@mail.ru>
2019-02-07 11:17:50 +0600

    upd

---
+++ w2/Lab2/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Print_info(FileSystemInfo fi, int nmspc)
+        {
+            string s = new string(' ', nmspc);
+            s = s + fi.Name;
+            Console.WriteLine(s);
+
+            if ( fi.GetType() ==  typeof(DirectoryInfo))
+            {
+                var v = (fi as DirectoryInfo).GetFileSystemInfos();
+                foreach (var n in v)
+                {
+                    Print_info(n, nmspc + 3);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\DzSee\Desktop\64");
+            Print_info(di, 1);
+        }
+    }
+}
---
+++ w2/Lab2/task3_new/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Directories = new DirectoryInfo(@"C:\Users\DzSee\Desktop\64").GetDirectories("*.*", SearchOption.AllDirectories);
+            // Directories.ToList();
+            foreach(string f in Directories)
+            {
+                Console.WriteLine(Path.GetFileName(f));
+            }
+              //  Console.WriteLine(Path.GetFile(Dir));
+        }
+    }
+}
